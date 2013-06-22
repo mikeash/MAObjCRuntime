049ac97e72cd1cfa4965da1b967db14da7a2cf60
Jonathan Mitchell <jonathan@mugginsoft.com>
2013-06-22 21:51:29 +0100

    Added OS X library target and tests.

---
+++ xcode/MAObjCRuntime_OSXTests/MAObjCRuntime_OSXTests.h
@@ -0,0 +1,13 @@
+//
+//  MAObjCRuntime_OSXTests.h
+//  MAObjCRuntime_OSXTests
+//
+//  Created by Jonathan on 22/06/2013.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MAObjCRuntime_OSXTests : SenTestCase
+
+@end
